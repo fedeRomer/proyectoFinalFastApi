c87445a1877d9b347fd4140c5d9d7634e88c5551
Fede <22fede22@gmail.com>
2022-06-15 21:46:30 -0300

    add connection to database and post method to query the db table names

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-from typing import Union
+import sqlalchemy
 
+from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from sqlalchemy.ext.declarative import declarative_base
+
+##requirement: 
+# pip install 
+#               PyMySQL
+#               SQLAlchemy
+#               fastapi
+#               "uvicorn[standard]"
 
 app = FastAPI()
+
+#German: server USAL
+#IP check discord
+#Usuario check discord
+#Password check discord
+
+#User mysql: check discord
+#Pass mysql: check discord
+
+# Define the engine using MYSQL Connector/Python
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://USER:PASSWORD@***check discord:3306/te_pilar_grp1'
+# Check Conn
+engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 
 
 @app.get("/")
@@ -13,3 +37,11 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/querytest")
+def run_query():
+
+    
+    
+    return {print(engine.table_names())}
