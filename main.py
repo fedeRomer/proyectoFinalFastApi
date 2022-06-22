0c44ce17644403de0e5936d35cc630fc0961da6b
Fede <22fede22@gmail.com>
2022-06-22 19:47:16 -0300

    t

--- main.py
+++ main.py
@@ -28,6 +28,33 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://USER:PASSWORD@***check discord:3306/t
 # Check Conn
 engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 
+##
+""" Consigna para tener resuelta para la semana que viene
+
+Incorporar registro temporal de las lecturas y ubicación
+
+Desarrollar los siguientes métodos para la API TEMP
+
+Método max_temp: devuelte el valor máximo de temperatura
+
+Método max_hum: devuelve el valor máximo de humedad
+
+Método min_temp: devuelte el valor mínimo de temperatura
+
+Método min_hum: devuelve el valor mínimo de humedad
+
+Método temp_max_by_qty: devuele la cantidad de registros de temperatura máximas solicitados
+
+Método hum_max_by_qty: devuele la cantidad de registros de humedad máximas solicitados
+
+Método temp_min_by_qty: devuele la cantidad de registros de temperatura mínimas solicitados
+
+Método hum_min_by_qty: devuele la cantidad de registros de humedad mínimas solicitados
+
+Método temp_by_location: devuelve los últimos 10 registros de temperatura de la ubicación especificada
+
+Método hum_by_location: devuelve los últimos 10 registros de humedad de la ubicación especificada
+ """
 
 @app.get("/")
 def read_root():
@@ -45,3 +72,9 @@ def run_query():
     
     
     return {print(engine.table_names())}
+
+@app.post("/saveTemperature")
+def save(temperature):
+
+    
+    return{"temperature": temperature}
